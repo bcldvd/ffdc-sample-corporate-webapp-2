1f12d515ec6e3c026f07f3450c618deace773ba8
David <david.bocle@finastra.com>
2020-05-20 14:58:09 +0200

    feat(global-nav): Update to latest

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 export class AppComponent {
   appName = 'Corporate Account Services';
 
-  demoAppList = routes;
+  navigationNodes = routes;
 
   constructor(public auth: AuthService, private router: Router) {}
 
